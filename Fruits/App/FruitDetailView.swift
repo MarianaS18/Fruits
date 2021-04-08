1dfae0882b7584777a37c52bffa35915ea9baae9
Mariana <mariana@fastmail.net>
2021-04-08 13:55:26 +0200

    create groupBox with link

---
+++ Fruits/App/FruitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Mariana Steblii on 08/04/2021.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
