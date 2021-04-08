8d09044dcc3370f6ee60b411c00b505bc6ed94f4
Mariana <mariana@fastmail.net>
2021-04-08 11:40:00 +0200

    work with data in the file

--- Fruits/FruitCardView.swift
+++ Fruits/FruitCardView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct FruitCardView: View {
     
     // MARK: - PROPERTIES
+    
     @State private var isAnimating: Bool = false
+    var fruit: Fruit
     
     // MARK: - BODY
     
@@ -18,21 +20,21 @@ struct FruitCardView: View {
         ZStack {
             VStack(spacing: 20) {
                 // FRUIT: image
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
                 
                 // FRUIT: title
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                 
                 // FRUIT: headline
-                Text("Blueberries are sweet, nutritious and widly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -49,7 +51,7 @@ struct FruitCardView: View {
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .center)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal, 20)
     }
@@ -58,7 +60,7 @@ struct FruitCardView: View {
     // MARK: - PREVIEW
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[4])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
