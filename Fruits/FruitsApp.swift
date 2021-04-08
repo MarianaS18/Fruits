c02fb91c61acfdfb4972cd74c7e20df1099b40ff
Mariana <mariana@fastmail.net>
2021-04-08 10:00:19 +0200

    add assets colors and images

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Mariana Steblii on 08/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
