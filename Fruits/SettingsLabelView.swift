a328fbbfacb146f823bb0c89ef54d763ba384a69
Mariana <mariana@fastmail.net>
2021-04-09 08:24:34 +0200

    create resusable settings label

--- Fruits/SettingsLabelView.swift
+++ Fruits/SettingsLabelView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(labelText.uppercased())
+                .fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        } //: HSTACK
     }
 }
 
+    // MARK: - PREVIEW
+
 struct SettingsLabelView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsLabelView()
+        SettingsLabelView(labelText: "Fruits", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
